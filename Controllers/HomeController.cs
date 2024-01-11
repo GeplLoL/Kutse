521a149bf03d845e92f8b679dbd83f6aa3b8ee53
opilane <opilane@HP-CZC2349HV0>
2024-01-11 10:02:55 +0200

    sdf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using kutse.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace kutse.Controllers
@@ -25,6 +27,41 @@ namespace kutse.Controllers
         public ActionResult ankeet()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult ankeet(Guest guest)
+        {
+            E_mail(guest);
+            if (ModelState.IsValid)
+            {
+                return View("Thanks",guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public void E_mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "nepridumalnazvaniepocht@gmail.com";
+                WebMail.Password = "rnlt mfvn ftjb usxu";
+                WebMail.From = "nepridumalnazvaniepocht@gmail.com";
+                WebMail.Send(guest.Email, "Vastus kutsele", guest.Name + " vastas " + ((guest.WillAttend ?? false) ? "tuleb peole " : "ei tule peole"));
+                ViewBag.Message = "Kiri on saatnud!";
+            }
+            catch
+            {
+                ViewBag.Message = "Mul on kahju! Ei saa kirja saada!!!";
+            }
+
+
         }
 
         public ActionResult About()
